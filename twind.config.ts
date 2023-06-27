9bb7c381b3369bfe0eaab7cd927e1cb66e347c54
Thi Dang <dkthi2901@gmail.com>
2023-06-28 02:50:18 +0700

    update twind config with theme

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,25 @@
 import { Options } from "$fresh/plugins/twind.ts";
+import { autoDarkColor } from "@twind/core";
+import * as colors from "twind/colors";
 
 export default {
+  rules: [
+    ["hidden", { display: "none" }],
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Geologica", "sans-serif"],
+    },
+    colors: {
+      primary: "#0074D9",
+      secondary: "#9EDFFF",
+      accent: "#00BFFF",
+      neutral: "#F5F8FA",
+    },
+    extend: {
+      colors,
+    },
+  },
   selfURL: import.meta.url,
+  darkColor: autoDarkColor,
 } as Options;
