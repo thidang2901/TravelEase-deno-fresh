41bd6cc8e77ebd62b6cfe5c3ae2f8726e48912a8
Thi Dang <dkthi2901@gmail.com>
2023-06-27 18:44:55 +0700

    remove npm:joi

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,33 +1,9 @@
-import joi from "npm:joi";
-
-const envVarsSchema = joi
-  .object()
-  .keys({
-    DENO_ENV: joi
-      .string()
-      .valid("production", "development", "test")
-      .required(),
-    PORT: joi.number().positive().default(8000),
-    DATABASE_URL: joi.string().required().description(
-      "Database's Connection string",
-    ),
-  })
-  .unknown();
-
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
-  .validate(Deno.env.toObject());
-
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
-
 export default {
   app: {
-    env: envVars.DENO_ENV,
-    port: envVars.PORT,
+    env: Deno.env.get("DENO_ENV"),
+    port: Number(Deno.env.get("PORT")),
   },
   database: {
-    uri: envVars.DATABASE_URL,
+    uri: Deno.env.get("DATABASE_URL"),
   },
 };
